fc239c22611d6f194ad31c0d858149413ab73fa7
Jeffrey Chen <jeffrey@hiis.org>
2015-10-16 17:08:51 +1100

    minor fix

--- loader_AU1000036_20150531.sql
+++ loader_AU1000036_20150531.sql
@@ -22,7 +22,7 @@ BEGIN
     CONSTRAINT refset_language_pkey PRIMARY KEY(id, effectivetime, active)
   ) INHERITS (refset);
   EXECUTE 'COPY refset_language(id, effectivetime, active, moduleid, refsetid, referencedcomponentid, acceptabilityid) FROM '''
-        || folder || '\Refset\Language\der2_cRefset_Language' || type || '-en-AU_' release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
+        || folder || '\Refset\Language\der2_cRefset_Language' || type || '-en-AU_' || release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
   
   CREATE TABLE refset_refetdescriptor(
     attributedescription bigint not null,
@@ -31,7 +31,7 @@ BEGIN
     CONSTRAINT refset_refetdescriptor_pkey PRIMARY KEY(id, effectivetime, active)
   ) INHERITS (refset);
   EXECUTE 'COPY refset_refetdescriptor(id, effectivetime, active, moduleid, refsetid, referencedcomponentid, attributedescription, attributetype, attributeorder) FROM '''
-        || folder || '\Refset\Metadata\der2_cciRefset_RefsetDescriptor' || type || '-en-AU_' release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
+        || folder || '\Refset\Metadata\der2_cciRefset_RefsetDescriptor' || type || '-en-AU_' || release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
   
   CREATE TABLE refset_descriptiontype(
     descriptionFormat bigint not null,
@@ -39,7 +39,7 @@ BEGIN
     CONSTRAINT refset_descriptiontype_pkey PRIMARY KEY(id, effectivetime, active)
   ) INHERITS (refset);
   EXECUTE 'COPY refset_refetdescriptor(id, effectivetime, active, moduleid, refsetid, referencedcomponentid, descriptionFormat, descriptionLength) FROM '''
-        || folder || '\Refset\Metadata\der2_ciRefset_DescriptionType' || type || '-en-AU_' release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
+        || folder || '\Refset\Metadata\der2_ciRefset_DescriptionType' || type || '-en-AU_' || release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
   
   CREATE TABLE refset_ModuleDependency(
     sourceEffectiveTime date not null,
@@ -47,5 +47,5 @@ BEGIN
     CONSTRAINT refset_ModuleDependency_pkey PRIMARY KEY(id, effectivetime, active)
   ) INHERITS (refset);
   EXECUTE 'COPY refset_refetdescriptor(id, effectivetime, active, moduleid, refsetid, referencedcomponentid, sourceEffectiveTime, targetEffectiveTime) FROM '''
-        || folder || '\Refset\Metadata\der2_ssRefset_ModuleDependency' || type || '-en-AU_' release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
+        || folder || '\Refset\Metadata\der2_ssRefset_ModuleDependency' || type || '-en-AU_' || release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
 END $$;
