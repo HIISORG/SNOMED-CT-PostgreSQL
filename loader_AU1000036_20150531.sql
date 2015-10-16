a2139842ef24dfc9e2479ba45c78ba1be7251be2
Jeffrey Chen <jeffrey@hiis.org>
2015-10-16 16:53:06 +1100

    Load SNOMED CT AU 20150531 release files

--- loader_AU1000036_20150531.sql
+++ loader_AU1000036_20150531.sql
@@ -2,18 +2,21 @@ set schema 'snomedct';
 
 DO $$
 DECLARE 
-  folder TEXT := 'C:\';
+  folder TEXT := 'C:\SNOMED_CT-AU';
   type TEXT := 'Delta';
   release TEXT := 'AU1000036_20150531';
 BEGIN
+  -- Load terminology tables
   EXECUTE 'COPY concept(id, effectivetime, active, moduleid, definitionstatusid) FROM ''' 
         || folder || '\Terminology\sct2_Concept_' || type || '_' release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
   EXECUTE 'COPY description(id, effectivetime, active, moduleid, conceptid, languagecode, typeid, term, casesignificanceid) FROM '''
         || folder || '\Terminology\sct2_Description_' || type || '_' release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
   EXECUTE 'COPY relationship(id, effectivetime, active, moduleid, sourceid, destinationid, relationshipgroup, typeid,characteristictypeid, modifierid) FROM '''
         || folder || '\Terminology\sct2_Relationship_' || type || '_' release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
-        
-  DROP TABLE IF EXISTS refset_language CASCADE;
+  
+  -- Remove all existing refset tables.      
+  DROP TABLE IF EXISTS refset CASCADE;
+  -- Load refset tables
   CREATE TABLE refset_language(
     acceptabilityid bigint not null,
     CONSTRAINT refset_language_pkey PRIMARY KEY(id, effectivetime, active)
@@ -21,7 +24,6 @@ BEGIN
   EXECUTE 'COPY refset_language(id, effectivetime, active, moduleid, refsetid, referencedcomponentid, acceptabilityid) FROM '''
         || folder || '\Refset\Language\der2_cRefset_Language' || type || '-en-AU_' release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
   
-  DROP TABLE IF EXISTS refset_refetdescriptor CASCADE;
   CREATE TABLE refset_refetdescriptor(
     attributedescription bigint not null,
     attributetype bigint not null,
@@ -31,7 +33,6 @@ BEGIN
   EXECUTE 'COPY refset_refetdescriptor(id, effectivetime, active, moduleid, refsetid, referencedcomponentid, attributedescription, attributetype, attributeorder) FROM '''
         || folder || '\Refset\Metadata\der2_cciRefset_RefsetDescriptor' || type || '-en-AU_' release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
   
-  DROP TABLE IF EXISTS refset_descriptiontype CASCADE;
   CREATE TABLE refset_descriptiontype(
     descriptionFormat bigint not null,
     descriptionLength integer not null,
@@ -40,7 +41,6 @@ BEGIN
   EXECUTE 'COPY refset_refetdescriptor(id, effectivetime, active, moduleid, refsetid, referencedcomponentid, descriptionFormat, descriptionLength) FROM '''
         || folder || '\Refset\Metadata\der2_ciRefset_DescriptionType' || type || '-en-AU_' release || '.txt'' WITH (FORMAT csv, HEADER true, DELIMITER ''	'')';
   
-  DROP TABLE IF EXISTS refset_ModuleDependency CASCADE;
   CREATE TABLE refset_ModuleDependency(
     sourceEffectiveTime date not null,
     targetEffectiveTime date not null,
